e542632605a4788dd1fbdb66fb845b21c53d15c6
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-04-05 18:18:12 -0500

    Added Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,22 @@ import react from "@astrojs/react";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    image(),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "dracula" },
-      remarkPlugins: [remarkToc],
-      rehypePlugins: [rehypeMinifyHtml],
-      remarkRehype: { footnoteLabel: "Footnotes" },
-      gfm: false,
-    }),
-  ],
+  integrations: [react(), image(), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "dracula"
+    },
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeMinifyHtml],
+    remarkRehype: {
+      footnoteLabel: "Footnotes"
+    },
+    gfm: false
+  })],
+  output: "server",
+  adapter: vercel()
 });
