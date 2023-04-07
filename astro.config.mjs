f3124a4718897f28c6ebbbb94e1e93215f2b4bd4
Bryce Russell <brycetrussell@gmail.com>
2023-04-07 13:09:46 -0500

    Switch deploy to static
    Switch from serverless (SSR) to static (SSG) Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   })],
-  output: "server",
   adapter: vercel()
 });
